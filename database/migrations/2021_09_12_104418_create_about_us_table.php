8ae49b35030deb160f18e93d1554a2310d5f5f74
Arifuzzaman31 <Arifuzzaman.rb@gmail.com>
2021-09-15 12:22:02 +0600

    first add to branch

---
+++ database/migrations/2021_09_12_104418_create_about_us_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAboutUsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('about_us', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('image_one')->nullable();
+            $table->string('image_two')->nullable();
+            $table->string('image_three')->nullable();
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('about_us');
+    }
+}
